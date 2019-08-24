003aade6383fab4c0139bd306c30542f185df271
sprusr <scott@spru.sr>
2019-08-24 15:40:13 +0200

    Implement some event mappings

--- src/mappings/colonyNetwork.ts
+++ src/mappings/colonyNetwork.ts
@@ -1,6 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  IColonyNetwork,
   ColonyNetworkInitialised,
   TokenLockingAddressSet,
   MiningCycleResolverSet,
@@ -16,78 +14,11 @@ import {
   UserLabelRegistered,
   ColonyLabelRegistered
 } from "../../generated/ColonyNetwork/IColonyNetwork"
-import { ExampleEntity } from "../../generated/schema"
+import { Colony, Domain, ColonyRoles } from "../../generated/schema"
 
 export function handleColonyNetworkInitialised(
   event: ColonyNetworkInitialised
-): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.resolver = event.params.resolver
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.numRecoveryRoles(...)
-  // - contract.isInRecoveryMode(...)
-  // - contract.supportsInterface(...)
-  // - contract.getReplacementReputationUpdateLogEntry(...)
-  // - contract.getReplacementReputationUpdateLogsExist(...)
-  // - contract.getMetaColony(...)
-  // - contract.getColonyCount(...)
-  // - contract.isColony(...)
-  // - contract.addSkill(...)
-  // - contract.getSkill(...)
-  // - contract.getSkillCount(...)
-  // - contract.getReputationMiningSkillId(...)
-  // - contract.getTokenLocking(...)
-  // - contract.createColony(...)
-  // - contract.getColony(...)
-  // - contract.getCurrentColonyVersion(...)
-  // - contract.getParentSkillId(...)
-  // - contract.getChildSkillId(...)
-  // - contract.getReputationMiningCycle(...)
-  // - contract.calculateMinerWeight(...)
-  // - contract.getColonyVersionResolver(...)
-  // - contract.getReputationRootHash(...)
-  // - contract.getReputationRootHashNNodes(...)
-  // - contract.getProfileDBAddress(...)
-  // - contract.lookupRegisteredENSDomain(...)
-  // - contract.addr(...)
-  // - contract.getENSRegistrar(...)
-  // - contract.getMiningResolver(...)
-  // - contract.getFeeInverse(...)
-}
+): void {}
 
 export function handleTokenLockingAddressSet(
   event: TokenLockingAddressSet
@@ -103,7 +34,27 @@ export function handleColonyVersionAdded(event: ColonyVersionAdded): void {}
 
 export function handleMetaColonyCreated(event: MetaColonyCreated): void {}
 
-export function handleColonyAdded(event: ColonyAdded): void {}
+export function handleColonyAdded(event: ColonyAdded): void {
+  const {
+    params: { colonyId, colonyAddress, token },
+    transaction: { from },
+  } = event
+  
+  const creatorRoles = new ColonyRoles(`${colonyAddress.toHex()}_1_${from.toHex()}`)
+  creatorRoles.user = from.toHex()
+  creatorRoles.domain = `${colonyAddress.toHex()}_1`
+  creatorRoles.administration = true
+
+  const rootDomain = new Domain(`${colonyAddress.toHex()}_1`)
+  rootDomain.index = 1
+  rootDomain.roles = [creatorRoles.id]
+
+  const colony = new Colony(colonyAddress.toHex())
+  colony.index = colonyId
+  colony.token = token.toHex()
+  colony.domains = [rootDomain.id]
+  colony.save()
+}
 
 export function handleSkillAdded(event: SkillAdded): void {}
 
@@ -125,4 +76,10 @@ export function handleUserLabelRegistered(event: UserLabelRegistered): void {}
 
 export function handleColonyLabelRegistered(
   event: ColonyLabelRegistered
-): void {}
+): void {
+  const { colony: colonyAddress, label } = event.params
+  const colony = Colony.load(colonyAddress.toHex())
+  if (!colony) return
+  colony.ensName = label.toString() // This needs to be converted
+  colony.save()
+}
